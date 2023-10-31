deb73956da6e0bf0a72eb8a41be6a95a9db679a6
Long.H <longkr.work@gmail.com>
2023-10-30 20:45:32 -0500

    practice: config postgresql and docker

--- src/main/java/com/bugblogs/BugsBlogApplication.java
+++ src/main/java/com/bugblogs/BugsBlogApplication.java
@@ -1,4 +1,4 @@
-package com.bugblogs.bugsblog;
+package com.bugblogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/bugblogs/bugsblog/controller/BlogController.java
+++ src/main/java/com/bugblogs/bugsblog/controller/BlogController.java
@@ -2,9 +2,17 @@ package com.bugblogs.bugsblog.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bugblogs.bugsblog.beans.Blog;
@@ -20,9 +28,30 @@ public class BlogController {
         return blogRepository.findAll();
     }
 
-    @GetMapping("/blogs/1")
-    public Blog getBlogDetails() {
-        return new Blog(2, "1234", "One Thousand and One Nightss", "one thouse story....");
+    @GetMapping("/blogs/{id}")
+    public Blog getBlogDetails(@PathVariable long id) {
+        Optional<Blog> blog = blogRepository.findById(id);
+
+        if (blog.isEmpty()) {
+            throw new RuntimeException("Blog cannot find: " + id);
+        }
+
+        return blog.get();
+    }
+
+    @PostMapping("/blogs")
+    public void createBlog(@RequestBody Blog blog) {
+        blogRepository.save(blog);
+    }
+
+    @PutMapping("/blogs/{id}")
+    public void updateBlog(@PathVariable long id, @RequestBody Blog blog) {
+        blogRepository.save(blog);
+    }
+
+    @DeleteMapping("/blogs/{id}")
+    public void deleteBlog(@PathVariable long id) {
+        blogRepository.deleteById(id);
     }
 
 }
